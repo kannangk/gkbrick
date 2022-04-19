0323c27991886d9f8211dc04c29dc9cafab49bb6
kannangk <kannan@aequalisys.com>
2022-04-19 15:41:53 +0530

    initial

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const { Route } = require('./core/Route')
 
 class App extends Route {
   init() {
-    // if (cluster.isMaster) {
-    //   let cpuCore = os.cpus().length
-    //   for (let i = 0; i < cpuCore; i++) {
-    //     cluster.fork()
-    //   }
-    //   cluster.on('online', (worker) => {
-    //     if (worker.isConnected()) console.log(`worker is active ${worker.process.pid}`)
-    //   })
+     if (cluster.isMaster) {
+       let cpuCore = os.cpus().length
+       for (let i = 0; i < cpuCore; i++) {
+         cluster.fork()
+       }
+       cluster.on('online', (worker) => {
+         if (worker.isConnected()) console.log(`worker is active ${worker.process.pid}`)
+       })
 
-    //   cluster.on('exit', (worker) => {
-    //     if (worker.isDead()) console.log(`worker is dead ${worker.process.pid}`)
-    //     cluster.fork()
-    //   })
-    // } else {
-    //   //init default route
-    //   app.use(super.init())
-    //   // listenint server port
-    //   http.createServer(app).listen(process.env.PORT)
-    // }
+       cluster.on('exit', (worker) => {
+         if (worker.isDead()) console.log(`worker is dead ${worker.process.pid}`)
+         cluster.fork()
+       })
+     } else {
+       //init default route
+       app.use(super.init())
+       // listenint server port
+       http.createServer(app).listen(process.env.PORT)
+     }
   }
 }
 
