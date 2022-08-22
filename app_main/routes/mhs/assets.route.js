1f514e0e8be761150fa3a892903e49b95067833d
kannangk <kannan@aequalisys.com>
2022-08-22 16:47:10 +0530

    added chains response

--- app_main/routes/mhs/assets.route.js
+++ app_main/routes/mhs/assets.route.js
@@ -10,7 +10,7 @@ class AssetRoute extends Controller{
     const { view } = this
     return this.get('/mhs/getAssets',(req,res)=>{
         console.log("API Hit")
-        var data='[{"id":"dot","chainId":"91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3","precision":10,"priceId":"polkadot","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Polkadot.svg"}]'
+        var data='[{"id":"giant","chainId":"91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3","precision":10,"priceId":"giant","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Polkadot.svg"}]'
         res.send(data)
     });
     
