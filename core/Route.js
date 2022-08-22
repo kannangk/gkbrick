2c40319664d5f3233e6f5809c5e3a58c4d4bb04e
kannangk <kannan@aequalisys.com>
2022-08-22 16:25:29 +0530

    added chains response

--- core/Route.js
+++ core/Route.js
@@ -11,6 +11,7 @@ const { AboutRoute } = require('routes/home/about.route')
 
 //route assets
 const { AssetRoute } = require('routes/mhs/assets.route')
+const { AssetChainsRoute } = require('routes/mhs/assetsChains.route')
 
 class Route {
   init() {
