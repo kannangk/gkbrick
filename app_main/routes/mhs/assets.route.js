9bad303d10ef3b3c9e380f1591ffd001a8c46af8
kannangk <kannan@aequalisys.com>
2022-08-22 17:01:10 +0530

    added chains response

--- app_main/routes/mhs/assets.route.js
+++ app_main/routes/mhs/assets.route.js
@@ -10,7 +10,7 @@ class AssetRoute extends Controller{
     const { view } = this
     return this.get('/mhs/getAssets',(req,res)=>{
         console.log("API Hit")
-        var data='[{"id":"giant","chainId":"91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3","precision":10,"priceId":"giant","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Polkadot.svg"}]'
+        var data='[{"id":"dot","chainId":"91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3","precision":10,"priceId":"polkadot","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Polkadot.svg"},{"id":"ksm","chainId":"b0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe","precision":12,"priceId":"kusama","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Kusama.svg"},{"id":"giant","chainId":"giant","precision":14,"priceId":"giant","icon":"https://raw.githubusercontent.com/soramitsu/fearless-utils/master/icons/chains/white/Westend.svg"}]'
         res.send(data)
     });
     
